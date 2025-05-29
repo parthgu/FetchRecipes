f4a3baa6bde37db4136f0aaebeb7ee92c2517f6a
parthgu <gparth02@hotmail.com>
2025-05-29 10:07:36 -0700

    Initial Commit

---
+++ FetchRecipies/FetchRecipiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipiesApp.swift
+//  FetchRecipies
+//
+//  Created by Parth Gupta on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
