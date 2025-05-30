099f35ea1d060bd3d816d77d70eec950c48e2a1c
parthgu <gparth02@hotmail.com>
2025-05-29 23:02:58 -0700

    Created constants for strings

---
+++ FetchRecipies/Shared/Constants/Strings.swift
@@ -0,0 +1,8 @@
+//
+//  Strings.swift
+//  FetchRecipies
+//
+//  Created by Parth Gupta on 5/29/25.
+//
+
+import Foundation
---
+++ FetchRecipies/Shared/Modifiers/CardStyle.swift
@@ -0,0 +1,8 @@
+//
+//  CardStyle.swift
+//  FetchRecipies
+//
+//  Created by Parth Gupta on 5/29/25.
+//
+
+import Foundation
